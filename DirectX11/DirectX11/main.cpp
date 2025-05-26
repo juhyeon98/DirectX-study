be859fb5e9be6ef1ef8d7034e7743e94d35a3b44
juhyeon <griiim134@gmail.com>
2025-05-27 07:09:55 +0900

    update: add System in main

--- DirectX11/DirectX11/main.cpp
+++ DirectX11/DirectX11/main.cpp
@@ -5,3 +5,24 @@
 * 3. 먼저 입력에서 추가 종속성에 d3d11.lib; dxgi.lib; d3dcompiler.lib; 추가
 * 4. 시스템의 서브 시스템에서 `/SUBSYSTEM:WINDOWS` 로 변경
 */
+
+#include "System.h"
+
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdShow)
+{
+	System* system;
+	bool result;
+
+	system = new System;
+
+	result = system->Initialize();
+	if (result)
+	{
+		system->Run();
+	}
+
+	system->Shutdown();
+	delete system;
+	system = 0;
+	return 0;
+}
