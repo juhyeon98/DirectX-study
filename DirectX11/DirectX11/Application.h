d1b4c520d2a077a6a8f340cbd503399e1cb3e8cd
juhyeon <griiim134@gmail.com>
2025-05-27 14:44:56 +0900

    fix: add D3D in Application

--- DirectX11/DirectX11/Application.h
+++ DirectX11/DirectX11/Application.h
@@ -1,7 +1,17 @@
+/*
+* Application은 렌더링을 해주는 역할로 실제 렌더링은 D3D가 해준다.
+* WinMain : entry point
+* └── System
+*		├── Input : 입력 처리 담당
+*		└── Application : 렌더링 담당
+*				└──D3D : Direct 3D - 실제 렌더링
+*/
+
 #ifndef _APPLICATION_H_
 #define _APPLICATION_H_
 
 #include <Windows.h>
+#include "D3D.h"
 
 const bool FULL_SCREEN = false;
 const bool VSYNC_ENABLE = true;
@@ -21,6 +31,7 @@ public:
 private:
 	bool Render();
 private:
+	D3D* mDirect3D;
 };
 
 #endif
