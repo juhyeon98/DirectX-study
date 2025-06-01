5a83480adc7972d4486e2d7227227843bf393600
juhyeon <griiim134@gmail.com>
2025-06-01 09:34:42 +0900

    feat: Create window

--- DirectX11/DirectX11/main.cpp
+++ DirectX11/DirectX11/main.cpp
@@ -6,23 +6,85 @@
 * 4. 시스템의 서브 시스템에서 `/SUBSYSTEM:WINDOWS` 로 변경
 */
 
-#include "System.h"
+#include <Windows.h>
+#include <iostream>
 
-int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdShow)
+LRESULT CALLBACK WindowProc(HWND hWnd,
+	UINT message,
+	WPARAM wParam,
+	LPARAM lParam);
+
+int WINAPI WinMain(	HINSTANCE hInstance,
+					HINSTANCE hPervInstance,
+					LPSTR lpCmdLine,
+					int nCmdShow )
 {
-	System* system;
-	bool result;
+	HWND hWnd;
 
-	system = new System;
+	// Window class 설정
+	// Window class 는 커널이 렌더링하고 이벤트 처리를 하기 위한 템플릿(메타데이터)
+	WNDCLASSEX wc;
+	::ZeroMemory(&wc, sizeof(WNDCLASSEX));
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpszClassName = TEXT("WindowClass1");
 
-	result = system->Initialize();
-	if (result)
+
+	RECT wr = { 0,0,500,400 };
+	// 클라이언트 영역을 기준으로 전체 윈도우 크기로 변환
+	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
+
+	// Window class 등록
+	::RegisterClassEx(&wc);
+	hWnd = CreateWindowEx(NULL,
+						TEXT("WindowClass1"),
+						TEXT("TEST"),
+						WS_OVERLAPPEDWINDOW,
+						300,
+						300,
+						wr.right - wr.left,
+						wr.bottom - wr.top,
+						NULL,
+						NULL,
+						hInstance,
+						NULL );
+	::ShowWindow(hWnd, nCmdShow);
+
+	MSG msg = { 0, };
+	// Windows 이벤트 및 메시지 처리
+	while (TRUE)
 	{
-		system->Run();
+		if (::PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			::TranslateMessage(&msg);
+			::DispatchMessage(&msg);
+			if (msg.message == WM_QUIT)
+			{
+				break;
+			}
+		}
+		else
+		{
+			// Game
+		}
 	}
 
-	system->Shutdown();
-	delete system;
-	system = 0;
-	return 0;
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		case WM_DESTROY:
+		{
+			::PostQuitMessage(0); // WM_QUIT 전송
+			return 0;
+		}break;
+	}
+	::DefWindowProc(hWnd, message, wParam, lParam);
 }
